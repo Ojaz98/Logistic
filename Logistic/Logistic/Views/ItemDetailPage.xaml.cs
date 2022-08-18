6e7bb9269effaf6f8b216642c44d5afa21678b4a
Oscar Alfaro <papacnqueso@gmail.com>
2022-08-17 23:27:20 -0700

    Agregar archivos de proyecto.

---
+++ Logistic/Logistic/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Logistic.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Logistic.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Logistic/Logistic/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Logistic.Models;
+using Logistic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Logistic.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
